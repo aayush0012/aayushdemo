f9c3409682b1bdf9deba8b266313ac5b53914626
Aayush Bhatt <bhattaayush2006@gmail.com>
2025-12-20 11:46:38 +0530

    Create Diagonal_Traversal.cpp

---
+++ DSA/Trees/Diagonal_Traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<int> diagonal(Node *root) {
+    vector<int>ans ;
+    if(root==NULL){
+        return ans ;
+    }
+   
+    queue<Node*>q; 
+    q.push(root) ;
+    while(!q.empty()){
+        Node*temp=q.front(); 
+        q.pop(); 
+        while(temp){
+            ans.push_back(temp->data);
+            if(temp->left){
+                q.push(temp->left); 
+            }
+            temp=temp->right; 
+            
+        }
+    }
+    return ans; 
+    }
+};
