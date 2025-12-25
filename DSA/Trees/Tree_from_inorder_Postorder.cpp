e993ab9400543f10a3d98a12a78c7cddfdc436a2
Aayush Bhatt <bhattaayush2006@gmail.com>
2025-12-25 12:40:38 +0530

    Add comments for tree construction method
    Added comments to clarify the approach for constructing a tree from inorder and postorder traversals.

---
+++ DSA/Trees/Tree_from_inorder_Postorder.cpp
@@ -0,0 +1,33 @@
+ // same as making tree from inorder and preorder but we start iterating frome end of the posorder and assign right subtree values first
+class Solution {
+  public:
+   int posi(vector<int> &inorder ,int element,map<int,int>&mp){
+      return mp[element] ; 
+      
+  } 
+  Node* solve(vector<int> &inorder, vector<int> &postorder, int &index, int start,int end,map<int,int> &mp){
+      //base case 
+      if(index<0|| start>end){
+          return NULL ;
+      }
+     int element =preorder[index--]; 
+     Node*root= new Node(element); 
+     int pos = posi(inorder,element,mp); 
+     root->right= solve(inorder,postorder,index, pos+1, end,mp ) ;
+      root->left = solve(inorder,postorder,index,start ,pos-1,mp);
+     return root; 
+  }
+    Node *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        // code here
+        int index= postorder.size()-1 ;
+        int start= 0; 
+        int end= inorder.size(); 
+      
+        map<int,int>mp; 
+        for(int i =0 ;i<inorder.size(); i++){ 
+            mp[inorder[i]]=i ;
+        } 
+         Node*ans= solve(inorder , postorder , index, start ,end,mp) ;
+        return ans ; 
+    }
+};
